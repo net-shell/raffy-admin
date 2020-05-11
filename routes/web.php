7d174ac59b53da2c4ac1880f72c1731880ea30a2
Boyan <ash@microweber.com>
2020-05-11 14:53:09 +0300

    more updates and endpoint working

--- routes/web.php
+++ routes/web.php
@@ -1,3 +1,11 @@
 <?php
 
+use Illuminate\Support\Facades\Route;
+use Illuminate\Http\Request;
+
 Voyager::routes();
+
+Route::post('rfid-endpoint', function(Request $request) {
+    var_dump($request->only('id', 'text'));
+    var_dump(base64_decode($request->input('id')));
+});
