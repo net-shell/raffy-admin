03d3d99cba104f479614bcd08280706df6348683
Boyan <ash@microweber.com>
2020-05-13 18:37:58 +0300

    live monitor update

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,10 +1,10 @@
-console.log('Loading...');
-
 require('./bootstrap');
 
 import Echo from "laravel-echo"
 
 window.io = require('socket.io-client');
+var moment = require('moment');
+moment.locale('bg');
 
 window.Echo = new Echo({
     broadcaster: 'socket.io',
@@ -14,9 +14,60 @@ window.Echo = new Echo({
 window.Echo.private(`App.Log`)
     .listen('TagLogged', (e) => {
         let el = document.getElementById('monitor-app');
-        if(!el) return;
+        if (!el) return;
         let log = e.log;
+        let newClass = 'attention-please well';
         let newChild = document.createElement('div');
-        newChild.innerHTML = '<div class="log-entry well row fadein"><div class=col-sm-3><img class=img-responsive src="/storage/' + log.user.avatar + '"></div><div class="details col-sm-9"><h3 class="name">' + log.user.name + '</h3><div class="time">' + log.created_at + '</div><div class="reader">' + log.reader.name + '</div></div></div> ';
+        let timestamp = moment(log.created_at).format('X');
+        newChild.innerHTML = '<div class="log-entry row ' + newClass + '"><div class=col-sm-3><img class=img-responsive src="/storage/' + log.user.avatar + '"></div><div class="details col-sm-9"><div class="reader pull-right badge">' + log.reader.name + '</div><h3 class="name">' + log.user.name + '</h3><div class="live-clock" data-timestamp="' + timestamp + '"><h4 class="live-diff"></h4><h3 class="live-time"></h3><h4 class="live-date"></h4></div></div></div> ';
         el.insertBefore(newChild, el.firstChild);
+        moment().format();
     });
+
+function updateClocks(selRoot) {
+    // Live diff
+    let els = document.querySelectorAll(selRoot + ' .live-diff');
+    if (!els) return;
+    for(var elDiff of els) {
+        let now = getParentClock(elDiff);
+        elDiff.innerHTML = moment(now).fromNow();
+    }
+    
+    // Live time
+    els = document.querySelectorAll(selRoot + ' .live-time');
+    if (!els) return;
+    for (var elTime of els) {
+        let now = getParentClock(elTime);
+        elTime.innerHTML = moment(now).format('HH:mm:ss');
+    }
+    
+    // Live date
+    els = document.querySelectorAll(selRoot + ' .live-date');
+    if (!els) return;
+    for(var elDate of els) {
+        let now = getParentClock(elDate);
+        elDate.innerHTML = moment(now).format('dddd L');
+    }
+}
+
+function getParentClock(el) {
+    let root = el.parentNode;
+    
+    let now = new Date();
+    if (root && root.dataset.timestamp && root.dataset.timestamp.length > 0) {
+        now = new Date(root.dataset.timestamp * 1000);
+    }
+    console.log(root, root.dataset.timestamp, now.toUTCString())
+
+    return now;
+}
+
+function initClocks() {
+    updateClocks('.live-clock');
+}
+
+setInterval(initClocks, 1000);
+
+// Init
+initClocks();
+moment().format();
--- resources/views/monitor.blade.php
+++ resources/views/monitor.blade.php
@@ -3,9 +3,19 @@
 @section('css')
     <style>
         .log-entry {
-            padding: 2em;
-            border: 1px solid #333;
+            padding: 0;
             margin: 1em 0;
+            border: 1px solid #333;
+        }
+        .log-entry.faded {
+            opacity: 0.5;
+        }
+        .log-entry > div {
+            padding: 0;
+            margin-bottom: 0 !important;
+        }
+        .log-entry .details {
+            padding: 1em;
         }
         .log-entry .img-responsive {
             max-height: 200px;
@@ -13,15 +23,10 @@
         .log-entry .reader {
             font-weight: bold;
         }
-        .fadein, .fadeout {
-            opacity: 0;
-            -moz-transition: opacity 0.4s ease-in-out;
-            -o-transition: opacity 0.4s ease-in-out;
-            -webkit-transition: opacity 0.4s ease-in-out;
-            transition: opacity 0.4s ease-in-out;
-        }
-        .fadein {
-            opacity: 1;
+
+        .big-clock .live-time {
+            color: #000;
+            font-size: 8em;
         }
     </style>
 @stop
@@ -29,19 +34,30 @@
 @section('content')
 <div class="container">
     <h1>Монитор на записите в реално време</h1>
-    <div id="monitor-app">
-        @foreach($logs as $log)
-        <div class="log-entry well row">
-            <div class=col-sm-3>
-                <img class=img-responsive src="/storage/{{ $log->user->avatar }}">
+    <div class="row">
+        <div class="col-sm-8">
+            <div id="monitor-app">
+                @foreach($logs as $log)
+                <div class="log-entry well row attention-please">
+                    <div class=col-sm-3>
+                        <img class=img-responsive src="/storage/{{ $log->user->avatar }}">
+                    </div>
+                    <div class="details col-sm-9">
+                        <div class="reader pull-right badge">{{ $log->reader->name }}</div>
+                        <h3 class="name">{{ $log->user->name }}</h3>
+                        <div class="live-clock" data-timestamp="{{ $log->created_at->timestamp }}">
+                            <h4 class="live-diff"></h4>
+                            <h3 class="live-time"></h3>
+                            <h4 class="live-date"></h4>
+                        </div>
+                    </div>
+                </div>
+                @endforeach
             </div>
-            <div class="details col-sm-9">
-                <h3 class="name">{{ $log->user->name }}</h3>
-                <div class="time">{{ $log->created_at }}</div>
-                <div class="reader">{{ $log->reader->name }}</div>
-            </div>
-        </div>';
-        @endforeach
-    </div>
+        </div>
+        <div class="live-clock big-clock col-sm-4 text-center">
+            <h1 class="live-time"></h1>
+            <h4 class="live-date"></h4>
+        </div>
 </div>
 @stop
